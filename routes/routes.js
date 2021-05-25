a9afb8a0be6520a4df709a949e5478702eae1afb
Christopher Patton <christopher.p@eystudios.com>
2021-05-25 16:06:41 -0400

    added simple a-z sorting

--- routes/routes.js
+++ routes/routes.js
@@ -22,12 +22,160 @@ var appRouter = function (app) {
         });
     });
 
+    
     // maximum value for sort order given by BC
     var newSortOrder = 2147483647;
+    var sortOrder;
+
+    app.get("/sort", function (req, res) {
+        res.status(200).send({
+            message: 'Sort will happen here'
+        });
+        console.log("this was hit");
+        api.get('products?inventory_level:greater=0&limit=5000').then(function (response) {
+
+            let totalPages2 = response.meta.pagination.total_pages;
+
+
+            for(var i = 1; i <= totalPages2; i++) {
+
+                // BC api v3 allows filter queries such as inventory_level:greater=0 which will return all in stock products 
+                api.get('products?inventory_level:greater=0&page='+i+'&limit=5000').then(function (productsThree) {
+                   // console.log("in stock products", productsTwo);
+                    // Looping through in stock products
+                    Object.keys(productsThree).forEach(function (inStockItem) {
+                       
+                        for (var j = 0; j < productsThree[inStockItem].length; j++)
+                            // Check if in stock product has sort number 0
+                            if(productsThree[inStockItem][j].sort_order === 0) {
+                               
+                                console.log("This product", productsThree[inStockItem][j].name, "sort order is changing");
+                                // Update sort order from 0 to corresponding a-z sort number
+                                //Setting variable to the first letter of the prodct name
+                                var productName =  productsThree[inStockItem][j].name;
+                                var productChar;          
+                                var productLetter;            
+                                if(productName.charAt(0) === "["){
+                                    productChar = productName.indexOf(']')+1;
+                                    productLetter = productName.charAt(productChar);
+                                } else {
+                                    productLetter = productsThree[inStockItem][j].name.charAt(0);
+                                }                                               
+            
+                                console.log(productLetter, "the firsletter");
+                                // setting sort order based on the first letter of the product name
+                                switch(productLetter){
+                                    case "A" || "a": 
+                                        sortOrder = 1;
+                                        break;
+                                    case "B" || "b": 
+                                        sortOrder = 2;
+                                        break; 
+                                    case "C" || "c": 
+                                        sortOrder = 3;
+                                        break;   
+                                    case "D" || "d": 
+                                        sortOrder = 4;
+                                        break;
+                                    case "E" || "e": 
+                                        sortOrder = 5;
+                                        break; 
+                                    case "F" || "f": 
+                                        sortOrder = 6;
+                                        break; 
+                                    case "G" || "g": 
+                                        sortOrder = 7;
+                                        break;
+                                    case "H" || "h": 
+                                        sortOrder = 8;
+                                        break; 
+                                    case "I" || "i": 
+                                        sortOrder = 9;
+                                        break; 
+                                    case "J" || "j": 
+                                        sortOrder = 10;
+                                        break;
+                                    case "K" || "k": 
+                                        sortOrder = 11;
+                                        break;
+                                    case "L" || "l": 
+                                        sortOrder = 12;
+                                        break;
+                                    case "M" || "m": 
+                                        sortOrder = 13;
+                                        break;
+                                    case "N" || "n": 
+                                        sortOrder = 14;
+                                        break;
+                                    case "O" || "o": 
+                                        sortOrder = 15;
+                                        break;
+                                    case "P" || "p": 
+                                        sortOrder = 16;
+                                        break;
+                                    case "Q" || "q": 
+                                        sortOrder = 17;
+                                        break;
+                                    case "R" || "r": 
+                                        sortOrder = 18;
+                                        break;
+                                    case "S" || "s": 
+                                        sortOrder = 19;
+                                        break;
+                                    case "T" || "t": 
+                                        sortOrder = 20;
+                                        break;
+                                    case "U" || "u": 
+                                        sortOrder = 21;
+                                        break;
+                                    case "V" || "v": 
+                                        sortOrder = 22;
+                                        break; 
+                                    case "W" || "w": 
+                                        sortOrder = 23;
+                                        break; 
+                                    case "X" || "x": 
+                                        sortOrder = 24;
+                                        break; 
+                                    case "Y" || "y": 
+                                        sortOrder = 25;
+                                        break;  
+                                    case "Z" || "z": 
+                                        sortOrder = 26;
+                                        break;
+                                    default: 
+                                        sortOrder = 27;
+                                        break;                            
+                                }    
+                                console.log(sortOrder);
+                                api.put('products/' + productsThree[inStockItem][j].id, {
+                                    sort_order: sortOrder 
+                                }).then(function (req, res) {
+                                    // console.log(req);
+                                }).catch((err) => {
+                                    console.log(err)
+                                });
+                            }
+                    });
+
+                // Return error if any
+                }).catch((err) => {
+                    console.log(err)
+                });
+            }
+        }).catch((err) => {
+            console.log(err)
+        });
+        
+    });
+    //Create Route to grab all instock products from an external api call
+    //route will look at all instock products get the first letter than sort accordingly.
 
     // Switched to BC api v3
 
     function outOfStockOrder() {
+
+
 
             api.get('products?inventory_level=0&limit=5000').then(function (response) {
 
@@ -93,10 +241,108 @@ var appRouter = function (app) {
                             for (var j = 0; j < productsTwo[inStockItem].length; j++)
                                 // Check if in stock product has the maximum sort order which means this product was recently restocked
                                 if(productsTwo[inStockItem][j].sort_order === newSortOrder) {
+                                   
                                     console.log("This product", productsTwo[inStockItem][j].name, "is now in stock, its inventory level is now", productsTwo[inStockItem][j].inventory_level);
                                     // Update recently restocked product with sort order or 0
+                                    //Setting variable to the first letter of the prodct name
+                                    var productName =  productsTwo[inStockItem][j].name;
+                                    var productChar;          
+                                    var productLetter;            
+                                    if(productName.charAt(0) === "["){
+                                        productChar = productName.indexOf(']')+1;
+                                        productLetter = productName.charAt(productChar);
+                                    } else {
+                                        productLetter = productsTwo[inStockItem][j].name.charAt(0);
+                                    }                                               
+                
+                                    console.log(productLetter, "the firsletter");
+                                    // setting sort order based on the first letter of the product name
+                                    switch(productLetter){
+                                        case "A" || "a": 
+                                            sortOrder = 1;
+                                            break;
+                                        case "B" || "b": 
+                                            sortOrder = 2;
+                                            break; 
+                                        case "C" || "c": 
+                                            sortOrder = 3;
+                                            break;   
+                                        case "D" || "d": 
+                                            sortOrder = 4;
+                                            break;
+                                        case "E" || "e": 
+                                            sortOrder = 5;
+                                            break; 
+                                        case "F" || "f": 
+                                            sortOrder = 6;
+                                            break; 
+                                        case "G" || "g": 
+                                            sortOrder = 7;
+                                            break;
+                                        case "H" || "h": 
+                                            sortOrder = 8;
+                                            break; 
+                                        case "I" || "i": 
+                                            sortOrder = 9;
+                                            break; 
+                                        case "J" || "j": 
+                                            sortOrder = 10;
+                                            break;
+                                        case "K" || "k": 
+                                            sortOrder = 11;
+                                            break;
+                                        case "L" || "l": 
+                                            sortOrder = 12;
+                                            break;
+                                        case "M" || "m": 
+                                            sortOrder = 13;
+                                            break;
+                                        case "N" || "n": 
+                                            sortOrder = 14;
+                                            break;
+                                        case "O" || "o": 
+                                            sortOrder = 15;
+                                            break;
+                                        case "P" || "p": 
+                                            sortOrder = 16;
+                                            break;
+                                        case "Q" || "q": 
+                                            sortOrder = 17;
+                                            break;
+                                        case "R" || "r": 
+                                            sortOrder = 18;
+                                            break;
+                                        case "S" || "s": 
+                                            sortOrder = 19;
+                                            break;
+                                        case "T" || "t": 
+                                            sortOrder = 20;
+                                            break;
+                                        case "U" || "u": 
+                                            sortOrder = 21;
+                                            break;
+                                        case "V" || "v": 
+                                            sortOrder = 22;
+                                            break; 
+                                        case "W" || "w": 
+                                            sortOrder = 23;
+                                            break; 
+                                        case "X" || "x": 
+                                            sortOrder = 24;
+                                            break; 
+                                        case "Y" || "y": 
+                                            sortOrder = 25;
+                                            break;  
+                                        case "Z" || "z": 
+                                            sortOrder = 26;
+                                            break;
+                                        default: 
+                                            sortOrder = 27;
+                                            break;                            
+                                    }    
+                                    console.log(sortOrder);
                                     api.put('products/' + productsTwo[inStockItem][j].id, {
-                                        sort_order: 0
+                                        sort_order: sortOrder 
                                     }).then(function (req, res) {
                                         // console.log(req);
                                     }).catch((err) => {
@@ -122,8 +368,9 @@ var appRouter = function (app) {
     // Running the app again after 1 hour
     setInterval(function () {
         outOfStockOrder();
-        console.log('Rerunning after 30 seconds have passed');
-    }, 120000);
+        console.log('Rerunning after 60 seconds have passed');
+    }, /*120000*/30000);
+
 
 };
 
